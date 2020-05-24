69b3beac9b5e4a70c039e89157e311a1479b53ab
Fabien Di Tore <fabien@ditore.ch>
2020-05-24 12:09:40 +0200

    ctd

--- autolinux/.br2-external.mk
+++ autolinux/.br2-external.mk
@@ -7,8 +7,8 @@ BR2_EXTERNAL_NAMES =
 BR2_EXTERNAL_DIRS = 
 BR2_EXTERNAL_MKS = 
 
-BR2_EXTERNAL_NAMES += autolinux
+BR2_EXTERNAL_NAMES += AUTOLINUX
 BR2_EXTERNAL_DIRS += /home/fabien/Documents/work/buildroot/autolinux
 BR2_EXTERNAL_MKS += /home/fabien/Documents/work/buildroot/autolinux/external.mk
-export BR2_EXTERNAL_autolinux_PATH = /home/fabien/Documents/work/buildroot/autolinux
-export BR2_EXTERNAL_autolinux_DESC = automatable netboot linux
+export BR2_EXTERNAL_AUTOLINUX_PATH = /home/fabien/Documents/work/buildroot/autolinux
+export BR2_EXTERNAL_AUTOLINUX_DESC = automatable netboot linux
